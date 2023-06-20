607f4bac7b9369acec85c54b3c221e13e1d8290b
seaweed78 <adhikarikshitiz12@gmail.com>
2023-06-20 17:54:16 +0545

    sound1

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let level = 1;
 btns.on("click", function () {
   let btnClicked = $(this);
   btnClicked.addClass("btnAction");
+  playSound(btnClicked);
   setTimeout(function () {
     btnClicked.removeClass("btnAction");
   }, 300);
@@ -13,4 +14,11 @@ btns.on("click", function () {
 // RANDOM NUMBER GENERATOR
 function randomNumGenerator(min, max) {
   return Math.floor(Math.random() * (max - min) + 1);
+}
+
+function playSound(btnClicked) {
+  let btnId = btnClicked.attr("id");
+
+  let audio = new Audio("sounds/" + btnId + ".mp3");
+  audio.play();
 }
