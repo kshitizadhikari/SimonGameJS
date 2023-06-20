266de48ed53c44f16c84afeb87f3597338e65378
seaweed78 <adhikarikshitiz12@gmail.com>
2023-06-20 18:25:19 +0545

    level 1

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 let btns = $(".btn");
 
+let btnArray = ["red", "blue", "green", "yellow"];
+let started = false;
 let level = 1;
+let gamePattern = [];
+let userPattern = [];
+
+$(document).on("keypress", function () {
+  if (!started) {
+    $("h1").text("Level: " + level);
+    startLevel(level);
+    started = true;
+  }
+});
 
 btns.on("click", function () {
   let btnClicked = $(this);
-  btnClicked.addClass("btnAction");
-  playSound(btnClicked);
-  setTimeout(function () {
-    btnClicked.removeClass("btnAction");
-  }, 300);
+  let btnName = btnClicked.attr("id");
+  playSound(btnName);
+  animateButton(btnName);
 });
 
 // RANDOM NUMBER GENERATOR
@@ -16,9 +26,24 @@ function randomNumGenerator(min, max) {
   return Math.floor(Math.random() * (max - min) + 1);
 }
 
-function playSound(btnClicked) {
-  let btnId = btnClicked.attr("id");
-
-  let audio = new Audio("sounds/" + btnId + ".mp3");
+function playSound(btnName) {
+  let audio = new Audio("sounds/" + btnName + ".mp3");
   audio.play();
+}
+
+function animateButton(btnName) {
+  let btnClass = $("." + btnName);
+  btnClass.addClass("btnAction");
+  setTimeout(function () {
+    btnClass.removeClass("btnAction");
+  }, 300);
+}
+
+function startLevel(currentLevel) {
+  for (var i = 1; i <= currentLevel; i++) {
+    let randomNum = randomNumGenerator(1, 4);
+    let btn = btnArray[randomNum];
+    playSound(btn);
+    animateButton(btn);
+  }
 }
